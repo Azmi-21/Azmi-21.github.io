868ea0770638daf957beeaf3be533b09528988b2
Azmi Abidi <a_abidi@live.concordia.ca>
2024-01-08 11:13:12 -0500

    Header component

--- my-personal-website/src/index.js
+++ my-personal-website/src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1> AZMI THE WEBSITE </h1>);
+root.render(<h1> AZMI THE WEBSITE 3</h1>);
